5a2c1d4b3154c0910b518e3313d3d1c5b61b110a
givenone <givenone1@gmail.com>
2020-03-04 11:30:44 +0900

    setting

--- binaryGradient.cpp
+++ binaryGradient.cpp
@@ -5,10 +5,10 @@ using namespace cv;
 using namespace std;
 
 const int imageNo = 6;
- 
-int main(int argc, char* argv[])
-{
 
+
+void albedo(IplImage**& binary_images, IplImage**& mixed, IplImage**& diffuse, IplImage**& specular)
+{
     IplImage *image[imageNo];
     IplImage *HSV[imageNo];
     vector<Mat> hsv_planes[imageNo];
@@ -23,12 +23,28 @@ int main(int argc, char* argv[])
     }
 
 
+}
 
+void normal()
+{}
 
+int main(int argc, char* argv[])
+{
 
+    IplImage **image;
+    image = new IplImage* [imageNo];
+    char filename[1024];
+    for(int index = 0; index < imageNo; index++)
+    {
+        sprintf(filename, "%d", index);
+        image[index] = cvLoadImage(filename);
+    }
+
+    CvMat** mixed_albedo, specular_albedo, diffuse_albedo;
+    cvMat()
     
-        namedWindow("image");
-        //imshow("image", img);
-        waitKey(0);
-        return 0;
+    namedWindow("image");
+    //imshow("image", img);
+    waitKey(0);
+    return 0;
 }
