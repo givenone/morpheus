3e09d528b5f3845922c4bb8e749d288c0ca8109d
givenone <givenone1@gmail.com>
2020-03-05 17:57:25 +0900

    Add mixed normal

--- binaryGradient.cpp
+++ binaryGradient.cpp
@@ -1,5 +1,12 @@
+/* 
+    Implementation of Diffuse-Specular Separation using Binary Spherical Gradient Illumination
+    Christos Kampouris
+    Stefanos Zafeiriou
+    Abhijeet Ghosh (Imperial College London, 2018)
+*/
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <bits/stdc++.h>
  
 using namespace cv;
 using namespace std;
@@ -29,16 +36,15 @@ void albedo(IplImage**& binary_images, CvMat**& mixed, CvMat*& diffuse, CvMat*& 
     Chroma[0] = cvCreateMat(width, height, CV_8UC1);
     Chroma[1] = cvCreateMat(width, height, CV_8UC1);
 
+    CvMat* Hue = cvCreateMat(width,height, CV_8UC1);
+    CvMat* Intensity = cvCreateMat(width,height, CV_8UC1);
+    CvMat* Saturation = cvCreateMat(width,height, CV_8UC1);
+    CvMat* Hue_c = cvCreateMat(width,height, CV_8UC1);
+    CvMat* Intensity_c = cvCreateMat(width,height, CV_8UC1);
+    CvMat* Saturation_c = cvCreateMat(width,height, CV_8UC1);
+
     for(int index = 0; index < imageNo/2; index++)
     {
-        CvMat* Hue = cvCreateMat(width,height, CV_8UC1);
-        CvMat* Intensity = cvCreateMat(width,height, CV_8UC1);
-        CvMat* Saturation = cvCreateMat(width,height, CV_8UC1);
-
-        CvMat* Hue_c = cvCreateMat(width,height, CV_8UC1);
-        CvMat* Intensity_c = cvCreateMat(width,height, CV_8UC1);
-        CvMat* Saturation_c = cvCreateMat(width,height, CV_8UC1);
-
         cvSplit(HSV[index*2], Hue, Saturation, Intensity, NULL );
         cvSplit(HSV[index*2+1], Hue_c, Saturation_c, Intensity, NULL);
 
@@ -59,6 +65,8 @@ void albedo(IplImage**& binary_images, CvMat**& mixed, CvMat*& diffuse, CvMat*& 
             }
         }
     }
+    cvReleaseMat(&Chroma[0]);cvReleaseMat(&Chroma[1]);
+    cvReleaseMat(&Hue);cvReleaseMat(&Intensity);cvReleaseMat(&Saturation);cvReleaseMat(&Hue_c);cvReleaseMat(&Intensity_c);cvReleaseMat(&Saturation_c);
 
     for(int i = 0; i < width; i++)
     {
@@ -79,8 +87,39 @@ void albedo(IplImage**& binary_images, CvMat**& mixed, CvMat*& diffuse, CvMat*& 
 
 }
 
-void normal()
-{}
+void normal(IplImage**& binary_images, CvMat*& mixed, CvMat*& diffuse, CvMat*& specular)
+{
+    for(int i=0; i<width; i++) // Compute Mixed Normal
+    {
+        for(int j=0; j<height; j++)
+        {
+            float normal[3];
+            for(int axis = 0; axis < 3; axis++)
+            {
+                CvScalar g = cvGet2D(binary_images[axis], i, j);
+                CvScalar c = cvGet2D(binary_images[axis], i, j);
+
+                float I_g = ((float)(g.val[0] + g.val[1] + g.val[2])) / 3;
+                float I_c = ((float)(c.val[0] + c.val[1] + c.val[2])) / 3;
+
+                normal[axis] = I_g - I_c;
+            }
+            
+            float norm = (float) sqrt(normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]);
+
+            float *data = (float*)cvPtr2D(mixed, i, j);
+            data[0] = normal[0] / norm; data[1] = normal[1] / norm; data[2] = normal[2] / norm; 
+        }
+    }
+
+    // Separate diffuse normal using Mallick's suv space
+
+
+    
+
+
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -107,6 +146,13 @@ int main(int argc, char* argv[])
     diffuse_albedo = cvCreateMat(width, height, CV_8UC3);
 
     albedo(image, mixed_albedo, diffuse_albedo, specular_albedo);
+
+    CvMat *mixed_normal, *specular_normal, *diffuse_normal;
+    mixed_normal = cvCreateMat(width, height, CV_32FC3);
+    specular_normal = cvCreateMat(width, height, CV_32FC3);
+    diffuse_normal = cvCreateMat(width, height, CV_32FC3);
+
+    normal(image, mixed_normal, diffuse_normal, specular_normal);
 
     namedWindow("image");
     cvShowImage("image", specular_albedo);
