56bf0239ca17587a22d27073757df764bf4e6232
givenone <givenone1@gmail.com>
2020-03-09 17:34:55 +0900

    Add Pattern Generation

--- Simulation/blender/pattern.py
+++ Simulation/blender/pattern.py
@@ -11,8 +11,8 @@ def BinaryPattern(lightList):
     y_c = [(100, x, y, z) if y < 0 else (0, x, y, z) for (a, x, y, z) in lightList]
     z = [(100, x, y, z) if z >= 0 else (0, x, y, z) for (a, x, y, z) in lightList]
     z_c = [(100, x, y, z) if z < 0 else (0, x, y, z) for (a, x, y, z) in lightList]
-    w = [(100, x, y, z) for (a, x, y, z) in lightList]
-    b = [(0, x, y, z) for (a, x, y, z) in lightList]
+    w = [(100, x, y, z) for (a, x, y, z) in lightList] # all on (white)
+    b = [(0, x, y, z) for (a, x, y, z) in lightList] # all off
     return [('x', x), ('x_c', x_c), ('y', y), ('y_c', y_c), ('z', z), ('z_c', z_c), ('w', w), ('b', b)]
 
 def GradientPattern(lightList) :
@@ -21,4 +21,13 @@ def GradientPattern(lightList) :
     # Multiview Face Capture using Polarized Spherical Gradient Illumination
     # ACM Trans. on Graphics (Proc. SIGGRAPH Asia) 2011 - Abhijeet Ghosh 
 
-    return []
+    x = [(100 * (x+1 / 2), x, y, z) for (a, x, y, z) in lightList]
+    x_c = [(-100 * (x-1 / 2), x, y, z) for (a, x, y, z) in lightList]
+    y = [(100 * (y+1 / 2), x, y, z) for (a, x, y, z) in lightList]
+    y_c = [(-100 * (y-1 / 2), x, y, z) for (a, x, y, z) in lightList]
+    z = [(100 * (z+1 / 2), x, y, z) for (a, x, y, z) in lightList]
+    z_c = [(-100 * (z-1 / 2), x, y, z) for (a, x, y, z) in lightList]
+
+    w = [(100, x, y, z) for (a, x, y, z) in lightList]
+    b = [(0, x, y, z) for (a, x, y, z) in lightList]
+    return [('x', x), ('x_c', x_c), ('y', y), ('y_c', y_c), ('z', z), ('z_c', z_c), ('w', w), ('b', b)]
